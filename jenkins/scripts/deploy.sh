cd0a27c73e635eca314d6557844a8059d675589e
Anna <annanggg22@gmail.com>
2023-11-14 14:56:37 +0800

    c

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/M3500QC/Desktop/Yr3Tri1/3103/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/M3500QC/Desktop/Yr3Tri1/3103/jenkins-php-selenium-test/src:/var/www/html -v /var/run/docker.sock:/var/run/docker.sock php:7.2-apache
+
 sleep 1
 set +x
 
 echo 'Now...'
 echo 'Visit http://151.192.145.165/ to see your PHP application in action.'
+
